deefaa1dbb3320b268468a91100de85860bc88e9
80Quattro <muzyczek2@gmail.com>
2022-10-06 00:45:42 +0200

    Change App from class component to functional component and implement useState() hook

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import React, { Component } from 'react';
+import React, { Component, useState, useEffect } from 'react';
 import NominatimAPI from './services/nominatim';
 import OpenrouteserviceAPI from './services/openrouteservice';
 import OpenMeteoAPI from './services/openMeteo';
@@ -6,44 +6,68 @@ import Location from './Models/Location';
 import Route from './Models/Route';
 import Weather from './Models/Weather';
  
-class App extends Component {
+function App() {
 
-    state = {
-        placeName: 'placeText'
-    };
+    const [place1Name, setPlace1Name] = useState('');
+    const [place2Name, setPlace2Name] = useState('');
 
-    handleInputChange = (e) => {
+    let place1 = null, place2 = null;
+
+
+    const handleInputChange = (e) => {
         const place = e.target.value;
         NominatimAPI.searchForLocation(place).then(response => {
+
             const locations = response.features.map(d => new Location(d));
-            console.log(locations);
-            //const display_name = response.features[0]?.properties.display_name;
-            const { name } = locations[0];
-            this.setState({ placeName: name });
+
+            if(locations.length > 0) {
+                
+                const { name } = locations[0];
+                if(e.target.name === 'place1') {
+                    place1 = locations[0];
+                    setPlace1Name(name);
+                }
+                else {
+                    place2 = locations[0];
+                    setPlace2Name(name);
+                }
+            }
+
         })
     }
 
-    componentDidMount() {
+    const handleGoButtonClick = () => {
+        console.log(place1);
+        OpenrouteserviceAPI.getRoute({ coordinates: [[8.681495,49.41461],[8.686507,49.41943]] }).then(response => {
+            const route = new Route(response);
+            //console.log(response);
+            //console.log(route);
+        });
+    }
+
+    useEffect(() => {
         /*OpenrouteserviceAPI.getRoute({ coordinates: [[8.681495,49.41461],[8.686507,49.41943]] }).then(response => {
             const route = new Route(response);
             console.log(response);
             console.log(route);
         });*/
-        OpenMeteoAPI.getWeather([8.681495,49.41461]).then(response => {
+        /*OpenMeteoAPI.getWeather([8.681495,49.41461]).then(response => {
             const weather = new Weather(response);
             console.log(response);
             console.log(weather);
-        })
-    }
+        })*/
+    }, []);
 
-    render() { 
-        return (
-            <React.Fragment>
-                <input type='text' onChange={this.handleInputChange} />
-                <p>{this.state.placeName}</p>
-            </React.Fragment>
-        );
-    }
+    return (
+        <React.Fragment>
+            <input type='text' name='place1' onChange={handleInputChange} />
+            <p>{place1Name}</p>
+            <input type='text' name='place2' onChange={handleInputChange} />
+            <p>{place2Name}</p>
+            <input type="button" value="Go!" onClick={handleGoButtonClick} />
+        </React.Fragment>
+    );
+
 }
  
  export default App;
